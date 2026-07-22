4dbee32e107665a06fc474103578a03935881c5e
Alejo Navarro <alejonavarro20@gmail.com>
2026-07-22 20:49:11 -0300

    Implementar modificación de stock y refactorizar validaciones

--- js/script.js
+++ js/script.js
@@ -1,25 +1,40 @@
 const inventario = [];
 
+// PANEL DE CARGA DE PRODUTOS AL SISTEMA
 const cajaCodigo = document.getElementById("code");
 const cajaDescripcion = document.getElementById("description");
 const cajaCantidadInicial = document.getElementById("cantidad-inicial");
 const btnAñadirProducto = document.getElementById("add");
 
+// TABLA PARA MOSTRAR EL INVENTARIO EN PANTALLA
 const sectorInventario = document.getElementById("inventario-body");
 
+// PANEL DE EDICIÓN DE PRODUCTOS
+const selectorDeProductos = document.getElementById("selector");
+const cajaCantidadAEditar = document.getElementById("amount");
+const btnAñadirStock = document.getElementById("add-amount");
+const btnRemoverStock = document.getElementById("remove-amount");
+
+// FUNCIÓN PARA ACTUALIZAR E IMPRIMIR NUESTRO INVENTARIO EN PANTALLA
 function renderizarInventario(){
     sectorInventario.innerHTML = "";
+    selectorDeProductos.innerHTML = `<option value="">Elija una opción...</option>`;
 
     for (const producto of inventario){
+        const opcion = document.createElement("option");
+
         const fila = document.createElement("tr");
         const celdaCodigo = document.createElement("td");
         const celdaDescripcion = document.createElement("td");
         const celdaCantidad = document.createElement("td");
 
+        
+        opcion.textContent = producto.codigo;
         celdaCodigo.textContent = producto.codigo;
         celdaDescripcion.textContent = producto.descripcion;
         celdaCantidad.textContent = producto.cantidad;
 
+        selectorDeProductos.appendChild(opcion);
         fila.appendChild(celdaCodigo);
         fila.appendChild(celdaDescripcion);
         fila.appendChild(celdaCantidad);
@@ -69,9 +84,57 @@ btnAñadirProducto.addEventListener("click", ()=>{
     inventario.push(producto);
     renderizarInventario();
 
+
     cajaCodigo.value = "";
     cajaDescripcion.value = "";
     cajaCantidadInicial.value = "";
+
     
     console.log(inventario);
+});
+
+// FUNCION PARA MODIFICAR STOCK DE UN PRODUCTO EN ESPECIFICO
+function modificarStock(tipo){
+    const codigo = selectorDeProductos.value;
+    const productoEncontrado = inventario.find(producto => producto.codigo === codigo);
+    const cantidad = Number(cajaCantidadAEditar.value.trim());
+    
+    if (cajaCantidadAEditar.value.trim() === ""){
+        alert("Ingrese un número valido");
+        return;
+    }
+    if (!productoEncontrado){
+        alert("Su producto no existe.");
+        return;
+    }
+    if (!Number.isInteger(cantidad)){
+        alert("Ingrese un valor entero.");
+        return;
+    }
+    if (cantidad <= 0){
+        alert("Ingrese un valor mayor a 0.");
+        return;
+    }
+    // SI SU PRODUCTO EXISTE: 
+    if (tipo === "añadir"){
+        productoEncontrado.cantidad += cantidad;
+    } else if (tipo === "remover"){
+        if ((productoEncontrado.cantidad - cantidad) < 0){
+            alert("Denegado, su stock queda en negativo.");
+            return;
+        }
+        productoEncontrado.cantidad -= cantidad;
+    }
+    renderizarInventario();
+    console.log(inventario);
+    cajaCantidadAEditar.value = "";
+};
+// BOTON PARA AÑADIR STOCK DE UN PRODCUTO EN ESPECIFICO
+btnAñadirStock.addEventListener("click", ()=>{
+    modificarStock("añadir");
+    
+});
+// BOTON PARA REMOVER STOCK DE UN PRODUCTO EN ESPECIFICO
+btnRemoverStock.addEventListener("click", ()=>{
+    modificarStock("remover")
 });
