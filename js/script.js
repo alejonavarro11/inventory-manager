80701d91112ff4abccb83a0dc1681fa2f7620ddd
Alejo Navarro <alejonavarro20@gmail.com>
2026-07-22 05:21:01 -0300

    Renderizar inventario en una tabla

--- js/script.js
+++ js/script.js
@@ -5,11 +5,37 @@ const cajaDescripcion = document.getElementById("description");
 const cajaCantidadInicial = document.getElementById("cantidad-inicial");
 const btnAñadirProducto = document.getElementById("add");
 
+const sectorInventario = document.getElementById("inventario-body");
+
+function renderizarInventario(){
+    sectorInventario.innerHTML = "";
+
+    for (const producto of inventario){
+        const fila = document.createElement("tr");
+        const celdaCodigo = document.createElement("td");
+        const celdaDescripcion = document.createElement("td");
+        const celdaCantidad = document.createElement("td");
+
+        celdaCodigo.textContent = producto.codigo;
+        celdaDescripcion.textContent = producto.descripcion;
+        celdaCantidad.textContent = producto.cantidad;
+
+        fila.appendChild(celdaCodigo);
+        fila.appendChild(celdaDescripcion);
+        fila.appendChild(celdaCantidad);
+        sectorInventario.appendChild(fila);
+    }
+
+    
+};
+
+// BOTON PARA AÑADIR PRODUCTOS AL INVENTARIO
 btnAñadirProducto.addEventListener("click", ()=>{
     const codigo = cajaCodigo.value.trim().toUpperCase();
     const descripcion = cajaDescripcion.value.trim();
     const cantidad = Number(cajaCantidadInicial.value);
 
+    
     if (codigo === ""){
         alert("Ingrese un código válido.");
         return;
@@ -41,10 +67,11 @@ btnAñadirProducto.addEventListener("click", ()=>{
     }
 
     inventario.push(producto);
+    renderizarInventario();
 
     cajaCodigo.value = "";
     cajaDescripcion.value = "";
     cajaCantidadInicial.value = "";
     
     console.log(inventario);
-})
+});
